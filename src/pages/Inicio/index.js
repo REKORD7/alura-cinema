56f2a588ca76d1838220bbf71473a766dcad9e6c
REKORD7 <godm8905@gmail.com>
2024-06-19 18:11:53 -0700

    Act aula 1

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,16 @@
+import Pie from "../../components/Pie/index";
+import Cabecera from "../../components/Cabecera/Cabecera";
+
+
+
+function Inicio() {
+    return (
+        <>
+            <Cabecera></Cabecera>
+
+            <Pie />
+        </>
+    );
+}
+
+export default Inicio;
